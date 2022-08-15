8812d1ce5bb123d56af2de7b3db4efab6eeb4653
Ashiful Bhuiyan <ashiful@conquestace.com>
2022-08-14 22:24:48 -0400

    Update common_commands.py

--- tools/common_commands.py
+++ tools/common_commands.py
@@ -656,5 +656,5 @@ class CommonCommands:
                 self.fights[message["username"]] = 0
             self.channel.send_message(message["username"] + " has defeated " + str(self.fights[message["username"]]) + " opponents.")
             
-     def discord(self, message):
-            self.channel.send_message("Come join the [https://www.conquestace.com/osutraining.html  osu!Training discord]")           
+    def discord(self, message):
+        self.channel.send_message("Come join the [https://www.conquestace.com/osutraining.html  osu!Training discord]")           
